5e74abe495691e93780f70dcb43c9b03ff8d2d99
Bradley Reed <shenmusha@yahoo.com>
2021-06-18 17:45:27 -0400

    regular saving interval

---
+++ conponents/addName.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from 'styled-components';
+
+export default function EmailAdderComponent( {setName} ){
+    const input = React.createRef('');
+    const EmailAdder = style.div`
+    
+    width: -webkit-fill-available;
+    height: 100vh;
+    position: absolute;
+    z-index: 2;
+    background: wheat;
+    top: 0;
+    `;
+
+    return
+}
