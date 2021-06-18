f95fdc3728403d6fea099cf0d23c6307b7b5c57e
Bradley Reed <shenmusha@yahoo.com>
2021-06-18 16:55:38 -0400

    changed file name, added component

--- conponents/message.jsx
+++ conponents/message.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import style from 'styled-components'
+import style from 'styled-components';
+import Message from './messageBox';
 
-export default function Message({data}){
-    const {message, sender} = data
+export default function Messages(){
 
-const Message = style.div`
-min-height: 21px;
-max-width: 90%;
-min-width: 50%;
-width: max-content;
-background: ${sender?'pink':'blue'};;
-text-align: left;
-padding: 10px;
-border-radius: 10px;
-color: white;
-margin: 15px 0;
-float: ${sender?'left':'right'}
-`
+    const Messages = style.div`
+    padding: 20px;
+    height: calc(100% - 64px);
+    overflow-y: scroll;
+    `
 
-    return(
-        <Message>
-          {message}
-        </Message>
+    const messages =[
+        {
+            message:'Hi',
+            sender:0,
+        },
+        {
+            message: 'Hi!',
+            sender:0,
+        },
+        {
+            message: 'Have you guys read any of my former articles?',
+            sender: 0,
+        }, 
+        {
+            message: 'Hi',
+            sender: 1,
+        },
+        {
+            message: 'Hi',
+            sender: 0,
+        },
+        {
+            message: 'Hi!',
+            sender: 1,
+        }
+    ]
+    return (
+        <Messages>{messages.map((data)=>{
+            return <Message data={data} />
+            })}
+        </Messages>
     )
 }
